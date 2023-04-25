05f9ab10270d530dda359ef8fe60fc82053f89ec
ras19 <ras19@ic.ac.uk>
2023-04-25 17:27:23 +0100

    Started segmenting images with SA. Need to run it on correct machine

---
+++ image-segmentation/segment_anything/segment_with_sa.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.insert(1, "..")
+from img_utils import load_img_and_convert_to_three_channels
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    polygons = []
+    color= []
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        img = np.ones((m.shape[0], m.shape[1], 3))
+        color_mask = np.random.random((1, 3)).tolist()[0]
+        for i in range(3):
+            img[:,:,i] = color_mask[i]
+        ax.imshow(np.dstack((img, m * 0.35)))
+
+def show_image(img, masks=None, with_masks=False):
+    plt.figure(figsize=(20,20))
+    plt.imshow(img)
+    if with_masks:
+        show_anns(masks)
+    plt.axis('off')
+    plt.show()
+
+def generate_masks(image, sam_checkpoint, model_type):
+    sam = sam_model_registry[model_type](sam_checkpoint)
+    sam.to(device="cuda")
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    masks = mask_generator.generate(image)
+        
+if __name__ == "__main__":
+    eg_img_path = "../waterworld_imgs/example.png"
+    image = load_img_and_convert_to_three_channels(eg_img_path)
+    # print(image)
+    sam_checkpoint = "/vol/bitbucket/ras19/se-model-checkpoints/sam_vit_h_4b8939.pth"
+    model_type="vit_h"
+    masks = generate_masks(image, sam_checkpoint, model_type)
+    show_image(image, masks, with_masks=True)
+    print("Done with getting the masks. Need to inspect them now")
