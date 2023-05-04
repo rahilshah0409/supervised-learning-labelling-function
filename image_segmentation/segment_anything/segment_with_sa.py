77b94d2dc0f590142de5944bc789f331593bec5f
ras19 <ras19@ic.ac.uk>
2023-05-04 12:41:20 +0100

    Tried SA on image where colliding takes place

--- image_segmentation/segment_anything/segment_with_sa.py
+++ image_segmentation/segment_anything/segment_with_sa.py
@@ -3,6 +3,7 @@ import math
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import pickle
 import sys
@@ -16,31 +17,37 @@ from img_utils import load_img_and_convert_to_three_channels
 ball_rad = 15 # default radius, be wary if this changes
 ball_area = math.pi * (ball_rad ** 2)
 
-def save_image_with_masks(img_original, masks):
-    print("Dimensions of original image")
-    print(img_original.shape[0])
-    print(img_original.shape[1])
-    print(img_original.shape[2])
-    original_image = Image.fromarray(img_original)
-    if len(masks) == 0:
-        return
-    sorted_anns = sorted(masks, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        img = np.ones((m.shape[0], m.shape[1], 3))
-        color_mask = np.random.random((1, 3)).tolist()[0]
-        for i in range(3):
-            img[:, :, i] = color_mask[i]
-        img_with_one_mask = np.dstack((img, m * 0.35))
-        print("Dimensions of image overlayed with a mask")
-        print(img_with_one_mask.shape[0])
-        print(img_with_one_mask.shape[1])
-        print(img_with_one_mask.shape[2])
-        # original_image.paste(img_with_one_mask)
-    # original_image.save("../saved_images/segment_anything.jpg")
+# def save_image_with_masks(img_original, masks):
+#     original_image = Image.fromarray(img_original)
+#     if len(masks) == 0:
+#         return
+#     sorted_anns = sorted(masks, key=(lambda x: x['area']), reverse=True)
+#     ax = plt.gca()
+#     ax.set_autoscale_on(False)
+#     for ann in sorted_anns:
+#         m = ann['segmentation']
+#         img = np.ones((m.shape[0], m.shape[1], 3))
+#         color_mask = np.random.random((1, 3)).tolist()[0]
+#         for i in range(3):
+#             img[:, :, i] = color_mask[i]
+#         img_with_one_mask = np.dstack((img, m * 0.35))
+#         print("Dimensions of image overlayed with a mask")
+#         print(img_with_one_mask.shape[0])
+#         print(img_with_one_mask.shape[1])
+#         print(img_with_one_mask.shape[2])
+#         # original_image.paste(img_with_one_mask)
+#     # original_image.save("../saved_images/segment_anything.jpg")
 
+def save_image_with_masks(masks, image, path):
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    plt.axis('on')
+    for mask in masks:
+        [x, y, width, height] = mask['bbox']
+        mask_boundary = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+        ax = plt.gca()
+        ax.add_patch(mask_boundary)
+    plt.savefig(path)
 
 def show_anns(anns):
     if len(anns) == 0:
@@ -76,14 +83,14 @@ def save_image_with_axis(img):
     plt.savefig("../eg_ww_img/example_with_axis.png")
 
 
-def generate_and_save_masks(image, sam_checkpoint, model_type):
+def generate_and_save_masks(image, sam_checkpoint, model_type, pkl_path):
     sam = sam_model_registry[model_type](sam_checkpoint)
     sam.to(device="cuda")
     mask_generator = SamAutomaticMaskGenerator(sam)
     masks = mask_generator.generate(image)
-    masks = filter_small_masks(masks, 100)
+    masks = filter_masks(masks, lower_uncertainty=100, upper_uncertainty=100)
     masks, centres = find_mask_colour(masks, image)
-    with open("../eg_ww_img/masks.pkl", "wb") as f:
+    with open(pkl_path, "wb") as f:
         pickle.dump(masks, f)
     return masks, centres
 
@@ -99,8 +106,8 @@ def inspect_masks(masks):
     print(first_mask['point_coords'])
 
 
-def filter_small_masks(masks, uncertainty):
-    filtered_masks = list(filter(lambda m: m['area'] >= ball_area - uncertainty and m['area'] <= ball_area + uncertainty, masks))
+def filter_masks(masks, lower_uncertainty, upper_uncertainty):
+    filtered_masks = list(filter(lambda m: m['area'] >= ball_area - lower_uncertainty and m['area'] <= ball_area + upper_uncertainty, masks))
     return filtered_masks
 
 
@@ -119,17 +126,22 @@ def find_mask_colour(masks, image):
 
 
 if __name__ == "__main__":
-    eg_img_path = "../eg_ww_img/example.png"
+    dir_path = "../eg_ww_img/"
+    orig_img_name = "example.png"
+    masks_pkl_filename = "masks.pkl"
+    img_with_masks_filename = "example_with_masks.png"
+    eg_img_path = dir_path + orig_img_name
     image = load_img_and_convert_to_three_channels(eg_img_path)
     print("Loaded and converted image to 3 channels")
     sam_checkpoint = "/vol/bitbucket/ras19/se-model-checkpoints/sam_vit_h_4b8939.pth"
     model_type = "vit_h"
-    # print("Generating masks using Segment Anything and saving them for later use")
-    # masks, centres = generate_and_save_masks(image, sam_checkpoint, model_type)
-    # print("Completed mask generation and masks are saved")
-    with open("../eg_ww_img/masks.pkl", "rb") as f:
+    pkl_path = dir_path + masks_pkl_filename
+    print("Generating masks using Segment Anything and saving them for later use")
+    masks, centres = generate_and_save_masks(image, sam_checkpoint, model_type, pkl_path)
+    print("Completed mask generation and masks are saved")
+    with open(pkl_path, "rb") as f:
         masks = pickle.load(f)
     # show_image(image, masks)
-    # save_image_with_masks(image, masks)
+    save_image_with_masks(masks, image, dir_path + img_with_masks_filename)
     # print("Inspecting masks produced")
     # inspect_masks(masks)
