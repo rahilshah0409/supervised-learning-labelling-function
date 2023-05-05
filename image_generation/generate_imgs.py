88a6a2102260b402992e95a941bd00589a3da080
ras19 <ras19@ic.ac.uk>
2023-05-05 09:47:42 +0100

    Regenerated snapshots from random and manual policy to include information about state vectors and episode lengths

--- image_generation/generate_imgs.py
+++ image_generation/generate_imgs.py
@@ -2,6 +2,7 @@
 import random
 import gym
 import os
+import pickle
 
 ball_area = None
 
@@ -10,19 +11,26 @@ def choose_action(seed):
     random.seed(seed)
     action = random.randint(0, NUM_ACTIONS - 1)
     return action
-
-def run_agent(env, num_episodes, random_seed=None):
-    ep_durs = []
-    states_traversed = []
+    
+# Note that if you wanted to run multiple traces, only the last trace will be saved because you will be overriding each image in the same directory (same name). Ideally want this functionality to move up to the function above. Move onto this later because we are only running one trace (but maybe have sub directory for each trace and corresponding images?)
+def run_rand_policy_and_save_traces(env, num_episodes, dir_path, img_base_filename, random_seed=None):
     events_per_episode = []
-    img_dir_path = "../image_segmentation/ww_trace_rand/"
-    base_filename = "env_rand_step"
+    trace_data_list = []
     for ep in range(num_episodes):
+        sub_dir_path = dir_path + "trace_" + str(ep)
+        if not os.path.exists(sub_dir_path):    
+            os.mkdir(sub_dir_path)
+        sub_dir_path = sub_dir_path + "/"
+        trace_data_i = {
+            "vectors": [],
+            "length": 0
+        }
+
         print("Episode {} in progress".format(ep + 1))
 
         state = env.reset()
         # Some rendering and saving of the image you get. INSERT HERE
-        env.save_and_render(img_dir_path, base_filename, 0)
+        env.save_and_render(sub_dir_path, img_base_filename, 0)
         states = []
         states.append(state)
 
@@ -35,18 +43,40 @@ def run_agent(env, num_episodes, random_seed=None):
                 random_seed += 2
             next_state, _, done, observations = env.step(action)
             t += 1
-            env.save_and_render(img_dir_path, base_filename, t)
+            env.save_and_render(sub_dir_path, img_base_filename, t)
             state = next_state
             states.append(state)
 
             # if (observations != set()):
             events_observed.append(observations)
             if done:
-                ep_durs.append((t, ep))
+                trace_data_i["length"] = t
+                trace_data_i["vectors"] = states
+                trace_data_list.append(trace_data_i)
 
-        states_traversed.append(states)
         events_per_episode.append(events_observed)
-    return ep_durs, states_traversed, events_per_episode
+
+    with open(dir_path + "traces_data.pkl", "wb") as f:
+        pickle.dump(trace_data_list, f)
+
+def save_traces_from_manual_play(env, num_episodes, dir_path, img_base_filename):
+    trace_data_list = []
+    for ep in range(num_episodes):
+        sub_dir_path = dir_path + "trace_" + str(ep)
+        if not os.path.exists(sub_dir_path):    
+            os.mkdir(sub_dir_path)
+        sub_dir_path = sub_dir_path + "/"
+        trace_data_i = {
+            "vectors": [],
+            "length": 0
+        }
+        _, states, _, _ = env.play(sub_dir_path, img_base_filename)
+        trace_data_i["length"] = len(states)
+        trace_data_i["vectors"] = states
+        trace_data_list.append(trace_data_i)
+
+    with open(dir_path + "traces_data.pkl", "wb") as f:
+        pickle.dump(trace_data_list, f)
 
 if __name__ == "__main__":
     use_velocities = False 
@@ -55,7 +85,12 @@ if __name__ == "__main__":
         "gym_subgoal_automata:WaterWorldRedGreen-v0",
         params={"generation": "random", "use_velocities": use_velocities, "environment_seed": 0},
     )
+    dir_path = "../image_segmentation/ww_trace/"
+    img_base_filename = "env_step"
+    random_seed = None
+    num_episodes = 1
+    # run_rand_policy_and_save_traces(env, num_episodes, dir_path, img_base_filename, random_seed)
+    save_traces_from_manual_play(env, num_episodes, dir_path, img_base_filename)
     # img_dir_path = "../image_segmentation/ww_trace/"
     # env.play(img_dir_path)
-    # run_agent(env, 1)
     # ball_area = env.get_ball_area()
