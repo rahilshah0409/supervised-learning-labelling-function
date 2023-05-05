af25ff921f72f895bc776dfd31d5e5d8e8254a99
ras19 <ras19@ic.ac.uk>
2023-05-05 15:37:58 +0100

    Experiment ran wrong, need to generate traces again

--- image_segmentation/dataset_labelling_pipeline.py
+++ image_segmentation/dataset_labelling_pipeline.py
@@ -6,13 +6,14 @@ from img_utils import load_img_and_convert_to_three_channels
 
 def generate_and_save_masks_for_eps(trace_data, trace_dir, sam_checkpoint, model_type, img_base_filename):
     num_eps = len(trace_data)
-    for ep in num_eps:
+    for ep in range(num_eps):
         print("Episode {} in progress".format(ep))
-        sub_dir = trace_dir + "trace_" + ep + "/"
+        sub_dir = trace_dir + "trace_" + str(ep) + "/"
         ep_len = trace_data[ep]["length"]
         masks_for_ep = []
         for step in range(ep_len):
-            image = load_img_and_convert_to_three_channels(sub_dir + img_base_filename + str(step))
+            image_loc = sub_dir + img_base_filename + str(step) + ".png"
+            image = load_img_and_convert_to_three_channels(image_loc)
             print("Starting to generate masks for step {} out of {}".format(step, ep_len - 1))
             masks = generate_and_filter_masks(image, sam_checkpoint, model_type)
             print("Finished generating masks for step {} out of {}".format(step, ep_len - 1))
@@ -24,20 +25,22 @@ def generate_and_save_masks_for_eps(trace_data, trace_dir, sam_checkpoint, model
 
 def generate_event_labels_from_masks(trace_data, trace_dir, masks_for_ep_filename, img_base_filename):
     num_eps = len(trace_data)
-    for ep in num_eps:
+    for ep in range(num_eps):
         print("Episode {} in progress".format(ep + 1))
         events_for_ep = []
-        sub_dir = trace_dir + "trace_" + ep + "/"
+        sub_dir = trace_dir + "trace_" + str(ep) + "/"
         ep_len = trace_data[ep]["length"]
         with open(sub_dir + masks_for_ep_filename, "rb") as f:
             masks_for_ep = pickle.load(f)
         # Still need to original image here to get the original vocab, can't use the masks alone. This worries me
-        image = load_img_and_convert_to_three_channels(sub_dir + img_base_filename + str(0))
+        first_image_loc = sub_dir + img_base_filename + str(0) + ".png"
+        first_image = load_img_and_convert_to_three_channels(first_image_loc)
         print("Step snapshot loaded.")
-        event_vocab = find_mask_colours(masks_for_ep[0], image)
+        event_vocab = find_mask_colours(masks_for_ep[0], first_image)
         print("Event vocab created.")
         for step in range(1, ep_len):
-            image = load_img_and_convert_to_three_channels(sub_dir + img_base_filename + str(step))
+            image_loc = sub_dir + img_base_filename + str(step) + ".png"
+            image = load_img_and_convert_to_three_channels(image_loc)
             events = get_event_occured(event_vocab, masks_for_ep[step], image)
             print("Events gathered for step {}".format(step))
             events_for_ep.append(events)
