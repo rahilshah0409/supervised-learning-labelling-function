cea1a64b6381d5527a3642adab0b4a3e7dc1286e
ras19 <ras19@ic.ac.uk>
2023-05-24 21:55:19 +0100

    Generated test dataset

--- labelling_function/label_func_framework.py
+++ labelling_function/label_func_framework.py
@@ -3,7 +3,7 @@ import sys
 from dataset_augmentation import get_dataset_for_model_train_and_eval
 sys.path.insert(1, "../")
 from image_segmentation.dataset_labelling_pipeline import generate_event_labels_from_masks
-from image_generation.generate_imgs import run_rand_policy_and_save_traces
+from image_generation.generate_imgs import run_rand_policy_and_save_traces, save_traces_from_manual_play
 from labelling_model import State2EventNet
 from model_training import eval_model, train_model
 import wandb
@@ -18,7 +18,8 @@ def generate_unlabelled_images(use_velocities, dataset_dir_path, img_base_fname)
     num_episodes = 5
 
     # Generate training data without labels (images and metadata)
-    run_rand_policy_and_save_traces(env, num_episodes, dataset_dir_path, img_base_fname, random_seed)
+    # run_rand_policy_and_save_traces(env, num_episodes, dataset_dir_path, img_base_fname, random_seed)
+    save_traces_from_manual_play(env, num_episodes, dataset_dir_path, img_base_fname)
 
 def label_dataset(img_dir_path, img_base_fname):
     # Segment the images with Segment Anything
@@ -48,7 +49,7 @@ def run_labelling_func_framework():
     # Generate training data
     train_data_dir = "/vol/bitbucket/ras19/fyp/dataset2/training/"
     img_base_fname = "step"
-    test_data_dir = "/vol/bitbucket/ras19/fyp/dataset2/test/"
+    test_data_dir = "../dataset/test/"
     test_img_base_fname = "test_step"
 
     # generate_unlabelled_images(use_velocities, train_data_dir, img_base_fname)
@@ -57,30 +58,30 @@ def run_labelling_func_framework():
     #     pickle.dump(events_captured, f)
 
     # Generate test data
-    # generate_unlabelled_images(use_velocities, test_data_dir, test_img_base_fname)
+    generate_unlabelled_images(use_velocities, test_data_dir, test_img_base_fname)
     # label_dataset(test_data_dir, test_img_base_fname)
 
     # TODO: Need to check quality of training and test dataset created by specified metrics
 
     # Should I be filtering the irrelevant events here?
-    with open("events_captured_3.pkl", "rb") as f:
-        events_captured = pickle.load(f)
-    events_captured_filtered = sorted(list(filter(lambda pair: pair[0] == "black" or pair[1] == "black", events_captured)))
+    # with open("events_captured_3.pkl", "rb") as f:
+    #     events_captured = pickle.load(f)
+    # events_captured_filtered = sorted(list(filter(lambda pair: pair[0] == "black" or pair[1] == "black", events_captured)))
     
-    # Create the model (i.e. learnt labelling function)
-    input_size = 52 if use_velocities else 28
-    output_size = 21 if use_velocities else 6
-    num_layers = 6
-    num_neurons = 64
-    labelling_fn = State2EventNet(input_size, output_size, num_layers, num_neurons)
+    # # Create the model (i.e. learnt labelling function)
+    # input_size = 52 if use_velocities else 28
+    # output_size = 21 if use_velocities else 6
+    # num_layers = 6
+    # num_neurons = 64
+    # labelling_fn = State2EventNet(input_size, output_size, num_layers, num_neurons)
 
-    learning_rate = 0.01
-    num_train_epochs = 500
-    train_batch_size = 32
-    test_batch_size = train_batch_size
+    # learning_rate = 0.01
+    # num_train_epochs = 500
+    # train_batch_size = 32
+    # test_batch_size = train_batch_size
     
-    # Get the training and test data from what has (already) been generated
-    train_data, train_label_distribution, test_data, test_label_distribution = get_dataset_for_model_train_and_eval(train_data_dir, events_captured_filtered, use_velocities, see_dataset=False)
+    # # Get the training and test data from what has (already) been generated
+    # train_data, train_label_distribution, test_data, test_label_distribution = get_dataset_for_model_train_and_eval(train_data_dir, events_captured_filtered, use_velocities, see_dataset=False)
 
     # Initialise weights and biases here
     # wandb.init(
@@ -120,7 +121,7 @@ def run_labelling_func_framework():
     #     print("Recall: {}".format(recall_scores[event]))
     #     print("F1: {}".format(f1_scores[event]))
 
-    return labelling_fn
+    # return labelling_fn
 
 if __name__ == "__main__":
     labelling_function = run_labelling_func_framework()
